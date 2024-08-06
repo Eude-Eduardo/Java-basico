dc3656aa89fd5d16bca3f13daf622b0d4f79608c
Eude Eduardo <eudeeduardo14@gmail.com>
2024-08-06 14:30:43 -0300

    conteudo de verdade

---
+++ JAVA-anatomia/src/edu/Eude/primeiraSemana/MinhaClasse.java
@@ -0,0 +1,15 @@
+package edu.Eude.primeiraSemana;
+public class MinhaClasse {
+
+public static void main (String [] args){
+    String primeiroNome = "Eude";
+    String segundoNome = "Eduardo";
+
+    String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+    System.out.print(nomeCompleto);
+}
+public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "Resultado do método " +primeiroNome.concat(" ").concat(segundoNome);
+
+    }
+}
