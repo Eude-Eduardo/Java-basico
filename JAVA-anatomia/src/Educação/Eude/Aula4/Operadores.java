bcf37779a566405b2725a3e085868087feb91d19
Eude Eduardo <eudeeduardo14@gmail.com>
2024-08-07 16:44:46 -0300

    java

--- JAVA-anatomia/src/Educação/Eude/Aula4/Operadores.java
+++ JAVA-anatomia/src/Educação/Eude/Aula4/Operadores.java
@@ -19,7 +19,7 @@ public class Operadores {
                 int multiplicação = 20 * 7;
                 int divisão = 15 / 3;
                 int restoDivisão = 18 % 3;
-                double resultado =  (10 * 7) + (20/4);
+                double res =  (10 * 7) + (20/4);
                 // () parenteses tem prioridade
                 /* 
                  */
@@ -57,6 +57,56 @@ public class Operadores {
         boolean fez = false;
             System.out.println("Antes: " + fez);
             System.out.println("Depois: inverteu o false (funciona se for true) " + !fez);
+            System.out.println("______________________________");
+        //Ternário
+        int a, b;
+
+        a = 5;
+        b = 6;
+        /*/* EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
+            if(a==b)
+            resultado = "verdadeiro";
+            else
+            resultado = "falso";
+            */
+        //MESMA CONDICIONAL, MAS DESSA VEZ, UTILIZANDO O OPERADOR CONDICIONAL TERNÁRIO
+            String resultado = (a==b) ? "verdadeiro" : "false";
+            System.out.println(resultado);
+            System.out.println("+_+_+_+_+_+_+_+");
+        // == Quando desejamos verificar se uma variável é IGUAL A outra.
+
+        /*!= Quando desejamos verificar se uma variável é DIFERENTE da outra.
+
+        > Quando desejamos verificar se uma variável é MAIOR QUE a outra.
+
+        >= Quando desejamos verificar se uma variável é MAIOR OU IGUAL a outra.
+
+        < Quando desejamos verificar se uma variável é MENOR QUE outra.
+
+        <= Quando desejamos verificar se uma variável é MENOR OU IGUAL a outra.*/
+
+            String nome1 = "Eude";
+            String nome2 = "eude";
+            
+            System.out.println(nome1 == nome2);
+            System.out.println(nome1 != nome2);
+            int n1 = 1;
+            int n2 = 2;
+
+            if (n1 == n2)//false
+                System.out.println(n1 + " é igual ao " + n2);
+            if (n1!=n2)// true
+                System.out.println(n1 + " é diferente de " + n2);
+            if (n1 < n2)// true
+                System.out.println(n1 + " é menor que " + n2);
+            if (n1 > n2)// false
+                System.out.println(n1 + " é maior que " + n2);
+            if (n1 <= n2)// true
+                System.out.println(n1 + " é menor ou igual a " + n2);
+            if (n1 >= n2)// false
+                System.out.println(n1 + " é maior ou igual a " + n2);
                 
-    }
+    }       
+    
+
 } 
